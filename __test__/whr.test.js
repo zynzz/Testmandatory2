a2a9e3779f2d080e5d92b0732c8e5e88e10979f5
zynzz <zynzz@zynzz.dk>
2018-12-06 10:32:04 +0100

    whr tests added

--- __test__/whr.test.js
+++ __test__/whr.test.js
@@ -12,8 +12,12 @@ describe('Waist-to-hip categories', () => {
     //Tests that are the same for male and female
     const generalTests = [
         {titel: 'Waist is not a number', waist: 'a', hip:110, outcome: error},
+        {titel: 'Waist and hip is not a number', waist: 'a', hip:'.', outcome: error},
         {titel: 'Hip is not a number', waist: 110, hip:'a', outcome: error},
-        {titel: 'Hip is 0', waist: 110, hip:0, outcome: error}
+        {titel: 'Hip is 0', waist: 110, hip:0, outcome: error},
+        {titel: 'Hip is A negative number', waist: 110, hip:-1, outcome: error},
+        {titel: 'Waist is A negative number', waist: -1, hip:110, outcome: error},
+        {titel: 'Hip and waist is negative numbers', waist: -1, hip:-1, outcome: error}
         ];
 
     //Tests
@@ -22,21 +26,21 @@ describe('Waist-to-hip categories', () => {
                 {titel: 'WHR is less than 0.90', waist: 97.9, hip:110, outcome: normalWeight},
                 {titel: 'WHR is 0.90', waist: 99, hip:110, outcome: overWeight},
                 {titel: 'WHR is 0.99', waist: 108.9, hip:110, outcome: overWeight},
-                {titel: 'WHR is greater than 1', waist: 110, hip:110, outcome: obesity}
+                {titel: 'WHR is greater than 0.99', waist: 110, hip:110, outcome: obesity}
         ]},
         {gender: 'Female', subtests: [
                 {titel: 'WHR is less than 0.80', waist: 86.9, hip:110, outcome: normalWeight},
                 {titel: 'WHR is 0.80', waist: 88, hip:110, outcome: overWeight},
                 {titel: 'WHR is 0.84', waist: 92.4, hip:110, outcome: overWeight},
-                {titel: 'WHR is greater than 0.85', waist: 93.5, hip:110, outcome: obesity},
+                {titel: 'WHR is greater than 0.84', waist: 93.5, hip:110, outcome: obesity},
         ]}];
 
-    for(test of tests){
-        test.subtests.push(...generalTests);
-        describe('WHR tests for ' + test.gender, () => {
-            for(subtest of test.subtests){
-                it(subtest.titel, () => {
-                    expect(whr(test.gender, subtest.waist, subtest.hip)).toBe(subtest.outcome);
+    for(let i = 0; i < tests.length; i++){
+        tests[i].subtests.push(...generalTests);
+        describe('WHR tests for ' + tests[i].gender, () => {
+            for(let j = 0; j < tests[i].subtests.length; j++){
+                it(tests[i].subtests[j].titel, () => {
+                    expect(whr(tests[i].gender, tests[i].subtests[j].waist, tests[i].subtests[j].hip)).toBe(tests[i].subtests[j].outcome);
                 }, timeout);
             }
         });
--- methods/whr.js
+++ methods/whr.js
@@ -1,3 +1,40 @@
-module.exports = function(gender, waist, hip){
+module.exports = function(gender, waist, hip) {
+    const normalWeight = 'Normal weight';
+    const overWeight = 'Overweight';
+    const obesity = 'Obesity';
+    const error = 'Error';
 
+    let whrCategory = error;
+
+    if (!isNaN(waist) && !isNaN(hip) && hip > 0 && waist > 0 ) {
+
+        let whr = waist / hip;
+        whr = whr.toFixed(2);
+
+    if (gender == 'Male') {
+        //Male
+        if (whr < 0.90) {
+            whrCategory = normalWeight;
+        } else if (whr >= 0.90 && whr <= 0.99) {
+            whrCategory = overWeight;
+        } else if (whr > 0.99) {
+            whrCategory = obesity;
+        }
+    } else if (gender == 'Female') {
+        //Female
+        if (whr < 0.80) {
+            whrCategory = normalWeight;
+        } else if (whr >= 0.80 && whr <= 0.84) {
+            whrCategory = overWeight;
+        } else if (whr > 0.84) {
+            whrCategory = obesity;
+        }
+    } else {
+        whrCategory = error;
+    }
+}else
+{
+    whrCategory = error;
+}
+return whrCategory;
 }
