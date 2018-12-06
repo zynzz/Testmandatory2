adf535edce4e7c6a7df2f7989bc1cbf4824cd01d
zynzz <zynzz@zynzz.dk>
2018-12-06 09:32:16 +0100

    Test bmi changed

--- __test__/bmicategory.test.js
+++ __test__/bmicategory.test.js
@@ -12,18 +12,18 @@ describe('Bmi categories', () => {
 
     //Contains all tests
     const tests = [
-        {titel:"BMI is less than 18.5", value: 18.4, outcome: underWeight},
+        {titel:"BMI is less than 18.5", value: 18.49, outcome: underWeight},
         {titel:"BMI is 18.5", value: 18.5, outcome: healthWeight},
         {titel:"BMI is 24.9", value: 24.9, outcome: healthWeight},
-        {titel:"BMI is 25", value: 25, outcome: overWeight},
+        {titel:"BMI is 24.91", value: 24.91, outcome: overWeight},
         {titel:"BMI is 29.9", value: 29.9, outcome: overWeight},
-        {titel:"BMI is 30 or higher", value: 30, outcome: obese},
+        {titel:"BMI is 29.91 or higher", value: 29.91, outcome: obese},
     ];
 
     //Tests
-    for(test of tests){
-        it(test.titel, () => {
-            expect(bmiCategory(test.value)).toBe(test.outcome);
+    for(let i = 0; i < tests.length; i++){
+        it(tests[i].titel, () => {
+            expect(bmiCategory(tests[i].value)).toBe(tests[i].outcome);
         }, timeout);
     }
 });
--- methods/bmicategory.js
+++ methods/bmicategory.js
@@ -1,3 +1,13 @@
 module.exports = function(bmi){
-
+    let bmiCategory = 'Error';
+    if(bmi < 18.5){
+        bmiCategory = 'Underweight';
+    }else if(bmi >= 18.5  &&  bmi <= 24.9){
+        bmiCategory = 'Health weight';
+    }else if(bmi > 24.9 && bmi <= 29.9){
+        bmiCategory = 'Overweight';
+    }else if(bmi > 29.9){
+        bmiCategory = 'Obese';
+    }
+    return bmiCategory;
 }
